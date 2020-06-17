429461b421f51954fe6ee8be2ddc341c0cefc1c0
TonyTang <tonythompsoncmu@gmail.com>
2020-06-16 21:40:55 -0700

    Revert "Dev"

--- app/src/main/java/com/demo/hellointent/MainActivity.java
+++ app/src/main/java/com/demo/hellointent/MainActivity.java
@@ -1,34 +1,14 @@
 package com.demo.hellointent;
 
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import android.os.Bundle;
 
-  public static final String LABEL = "label";
+public class MainActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
-  }
-
-  public void goToDeepLink(View view) {
-    Intent intent = new Intent();
-    intent.putExtra(LABEL, ((Button) view).getText());
-    intent.setData(Uri.parse("tony://detail/path"));
-    intent.setPackage(getPackageName());
-    startActivity(intent);
-  }
-
-  public void goToDefault(View view) {
-    Intent intent = new Intent(this, DefaultActivity.class);
-    intent.putExtra("label", ((Button) view).getText());
-    startActivity(intent);
   }
 }
