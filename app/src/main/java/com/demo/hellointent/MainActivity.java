711c29b145c51b9280ae39920aa92ccebb664f14
TonyTandAndroid <tonythompsoncmu@gmail.com>
2020-06-16 21:02:12 -0700

    Initial commit

---
+++ app/src/main/java/com/demo/hellointent/MainActivity.java
@@ -0,0 +1,14 @@
+package com.demo.hellointent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
